d13c3694f3921e136cc9d86999b081de7535b2bb
Vruthik Thakkar <vruthikthakkar@MacBook-Pro-14-inch-2112.local>
2022-03-25 14:21:27 -0400

    add eval and naivebayes

--- scrape.py
+++ scrape.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
+import pandas as pd
 
-def scrape_allsides():
-    r = requests.get('https://www.allsides.com/unbiased-balanced-news')
-    soup = BeautifulSoup(r.text, "html.parser")
-    
+def scrape():
+    # df = pd.read_csv('data/AllSides News Source Political Leanings.csv')
+    r = requests.get('https://www.axios.com/us-europe-liquefied-natural-gas-russia-d9f94ffe-1559-4307-8b13-28b4526beb02.html')
+    soup = BeautifulSoup(r.text, 'html.parser')
+    print(soup.text)
 
-
-scrape_allsides()
+if __name__ == "__main__":
+    # scrape(sys.argv[1])
+    scrape()
