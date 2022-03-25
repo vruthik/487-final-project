ea39591c33e42e6e83720ad06ca5e3b68f457c63
Vruthik Thakkar <vruthikthakkar@MacBook-Pro-14-inch-2112.local>
2022-03-25 13:13:29 -0400

    added requirements.txt

--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 def scrape_allsides():
     r = requests.get('https://www.allsides.com/unbiased-balanced-news')
     soup = BeautifulSoup(r.text, "html.parser")
-    print(soup)
+    
+
 
 scrape_allsides()
